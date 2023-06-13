14547e7414149888b29e1b24c78980f6a6fdc4f2
Xinya Chen <xinyachen@Normans-MacBook-Pro.local>
2023-06-13 09:40:50 -0700

    update nav sidebar

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -17,8 +17,8 @@ const NavigationSidebar = () => {
                         </span>
                     </Link>
                 )}
-                {!currentUser && <Link className="list-group" to="/tuiter/login">   Login   </Link>}
-                {!currentUser && <Link className="list-group" to="/tuiter/register">Register</Link>}
+                {!currentUser && <Link className="list-group-item" to="/tuiter/login">   Login   </Link>}
+                {!currentUser && <Link className="list-group-item" to="/tuiter/register">Register</Link>}
                 {/* {currentUser && <Link className="list-group" to="/tuiter/profile"> Profile </Link>} */}
 
             </div>
--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -2,7 +2,9 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as authService from "./auth-service";
 
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 
 export const createTuit = async (tuit) => {
